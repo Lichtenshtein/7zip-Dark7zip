9d0f9dc3ac7cbda527ab4cefc2c034131236c025
Nobody <36154203+PopuriAO29@users.noreply.github.com>
2024-01-14 03:40:09 +0000

    add-smart-extract-context-menu-option

--- CPP/7zip/UI/FileManager/MenuPage.cpp
+++ CPP/7zip/UI/FileManager/MenuPage.cpp
@@ -55,6 +55,7 @@ static const CContextMenuItem kMenuItems[] =
   { IDS_CONTEXT_EXTRACT, kExtract },
   { IDS_CONTEXT_EXTRACT_HERE, kExtractHere },
   { IDS_CONTEXT_EXTRACT_TO, kExtractTo },
+  { IDS_CONTEXT_EXTRACT_SMART, kExtractSmart },
 
   { IDS_CONTEXT_TEST, kTest },
 
