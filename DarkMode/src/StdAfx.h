1a0fee1ba3132ef4da691e4e81d04e6aa6459ee9
ozone10 <positronix10@gmail.com>
2025-10-06 00:13:55 +0200

    Update darkmodelib to 0.35.0

--- DarkMode/src/StdAfx.h
+++ DarkMode/src/StdAfx.h
@@ -1,1 +1,20 @@
 // StdAfx.h
+#ifndef ZIP7_INC_STDAFX_H
+#define ZIP7_INC_STDAFX_H
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
+#if defined(_MSC_VER) && _MSC_VER >= 1800
+#pragma warning(disable : 4464) // relative include path contains '..'
+#endif
+#include "../CPP/Common/Common.h"
+
+#endif
